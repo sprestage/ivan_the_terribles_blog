853ff8cf67b4d6c34a72a76e8b8c7533ce4cf939
Susan Prestage <susan.prestage@gmail.com>
2013-11-20 17:30:18 -0800

    improve performance with eager loading

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,7 +2,7 @@ class CommentsController < ApplicationController
   # GET /comments
   # GET /comments.json
   def index
-    @comments = Comment.all
+    @comments = Comment.all.includes(:post)
 
     respond_to do |format|
       format.html # index.html.erb
