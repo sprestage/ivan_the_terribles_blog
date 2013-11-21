9dff9de9c58d70cfcdb1ebcfc0e2cad8f754a848
Susan Prestage <susan.prestage@gmail.com>
2013-11-21 11:54:27 -0800

    implemented better eager loading for post/show page

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -13,7 +13,7 @@ class PostsController < ApplicationController
   # GET /posts/1
   # GET /posts/1.json
   def show
-    @post = Post.find(params[:id])
+    @post = Post.includes(:comments => :replies).find(params[:id])
 
     respond_to do |format|
       format.html # show.html.erb
